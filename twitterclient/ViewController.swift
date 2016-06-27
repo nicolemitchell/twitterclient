9aa569ad0af41ff55effc8b380f070d7146dd9e4
Nicole Mitchell <nicolemitchell@fb.com>
2016-06-27 13:45:51 -0700

    Initial Commit

---
+++ twitterclient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitterclient
+//
+//  Created by Nicole Mitchell on 6/27/16.
+//  Copyright © 2016 Nicole Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
