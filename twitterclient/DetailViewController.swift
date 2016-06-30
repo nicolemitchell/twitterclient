ed2020f9dd9ee46f22a2eb3217ee1cabe1b11507
Nicole Mitchell <nicolemitchell@fb.com>
2016-06-30 14:52:32 -0700

    detail page

--- twitterclient/DetailViewController.swift
+++ twitterclient/DetailViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var tweet: Tweet!
+
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var retweetButton: UIButton!
     @IBAction func onProfileButton(sender: AnyObject) {
     }
     @IBOutlet weak var nameLabel: UILabel!
@@ -18,12 +22,92 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var timestampLabel: UILabel!
     @IBOutlet weak var profpicImageView: UIImageView!
     
+    @IBOutlet weak var retweeterIcon: UIImageView!
+    
+    @IBOutlet weak var retweeterLabel: UILabel!
+    
+    @IBOutlet weak var favoriteCountLabel: UILabel!
+    @IBOutlet weak var retweetCountLabel: UILabel!
+    
+
+    @IBAction func onReply(sender: AnyObject) {
+    }
+    
+    @IBAction func onRetweet(sender: AnyObject) {
+        if tweet!.retweetBool == false {
+            tweet?.retweet({
+                self.retweetButton.selected = true
+                self.retweetCountLabel.text = String(self.tweet!.retweetCount)
+                }, failure: { (error: NSError) in
+                    
+            })
+        } else {
+            tweet?.unretweet({
+                self.retweetButton.selected = false
+                self.retweetCountLabel.text = String(self.tweet!.retweetCount)
+                }, failure: { (error:NSError) in
+                    
+            })
+        }
+    }
+    
+    @IBAction func onFavorite(sender: AnyObject) {
+        if tweet!.favoriteBool == false {
+            tweet?.favorite({
+                self.favoriteButton.selected = true
+                self.favoriteCountLabel.text = String(self.tweet!.favoritesCount)
+                }, failure: { (error: NSError) in
+                    
+            })
+        } else {
+            tweet?.unfavorite({
+                self.favoriteButton.selected = false
+                self.favoriteCountLabel.text = String(self.tweet!.favoritesCount)
+                }, failure: { (error: NSError) in
+                    
+            })
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        
+        if tweet != nil {
+            
+            self.tweetLabel.text = tweet.text as! String
+            self.nameLabel.text = tweet.name as! String
+            self.screennameLabel.text = "@" + (tweet.screenname as! String)
+            
+            if tweet.retweeter != nil {
+                retweeterLabel.hidden = false
+                retweeterIcon.hidden = false
+                retweeterLabel.text = (tweet.retweeter!.name as! String)+" Retweeted"
+            } else {
+                retweeterLabel.hidden = true
+                retweeterIcon.hidden = true
+                retweeterLabel.text = nil
+            }
+    
+            let timestamp = tweet.timestamp! as NSDate
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = NSDateFormatterStyle.ShortStyle
+            formatter.timeStyle = NSDateFormatterStyle.ShortStyle
+            let timeText = formatter.stringFromDate(timestamp) 
+            
+            self.timestampLabel.text = timeText
+            self.profpicImageView.setImageWithURL(tweet.profileUrl!)
+            
+            retweetCountLabel.text = String(tweet.retweetCount)
+            
+            favoriteCountLabel.text = String(tweet.favoritesCount)
+            
+            retweetButton.selected = tweet.retweetBool!
+            favoriteButton.selected = tweet.favoriteBool!
+            
+
+        }
         
     }
 
